e0c27407712204c33705183fa5bbb027bb1f7caa
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-06-01 19:24:07 -0300

    Second commit JPA/Hibernate working Maven

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,20 +1,27 @@
 package aplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "Joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 		
 		}
-
 	}
 
